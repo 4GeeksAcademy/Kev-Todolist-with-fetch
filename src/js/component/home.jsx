a3fde057f1d88dae461b0c160dc7a50acd87923e
FloresKev99 <165217315+FloresKev99@users.noreply.github.com>
2024-07-05 03:15:05 +0000

    Project done

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  return (
+    <div>
+      <h1>todos</h1>
+      <ul className="todolist">
+        <li>
+          <input
+            type="text"
+            onChange={(e) => setInputValue(e.target.value)}
+            value={inputValue}
+            onKeyPress={(e) => {
+              if (e.key === "Enter") {
+                setTodos(todos.concat([inputValue]));
+                setInputValue("");
+              }
+            }}
+            placeholder="What needs to be done?"
+          ></input>
+        </li>
+
+        {todos.map((item, index) => (
+          <li>
+            {item}{" "}
+            <FontAwesomeIcon
+              className="icon"
+              icon={faXmark}
+              onClick={() =>
+                setTodos(
+                  todos.filter((t, currentIndex) => index != currentIndex)
+                )
+              }
+            />
+          </li>
+        ))}
+
+        <div id="tasks-left">{todos.length} Tasks left</div>
+      </ul>
+      <div id="bottom-1"></div>
+      <div id="bottom-2"></div>
+    </div>
+  );
 };
 
 export default Home;
